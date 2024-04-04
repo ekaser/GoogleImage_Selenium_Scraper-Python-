029419cba0a4f2cd200ae28cafae05e038c57d86
ekaser <evan9347@gmail.com>
2024-04-04 08:05:52 -0400

    removed zip files

--- resources/env.py
+++ resources/env.py
@@ -1,11 +1,11 @@
 # Environment
-PATH = "C:\\Users\\evan9\\OneDrive\\Desktop\\school\\Applied Machine Learning\\project2\\scraper\\"
+PATH = "C:\\PATH\\TO\\scraper\\"
 IMAGE_DIR = "images\\"
 DATA_DIR = "data\\"
 MAX_IMAGES = 6
 CLASSES = ["cat"]
 BINARY_LOCATION = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-DRIVER = "resources\\driver.exe" 
+DRIVER = "resources\\driver.exe"
 #Output
 DEBUG = True #Do you want debug messages?
 VERBOSE = True #Do you want messages about the program?
